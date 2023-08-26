96389b983ee8ff775e8158e4cb4dd1cca8e501b1
deepak427 <62477872+deepak427@users.noreply.github.com>
2023-08-26 13:22:03 +0530

    llama creation completed

--- controllers/nutritionist.js
+++ controllers/nutritionist.js
@@ -1,21 +1,66 @@
-import { config } from "../config/config.js";
-import mysql from "mysql2"
+import { ClarifaiStub, grpc } from "clarifai-nodejs-grpc";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PAT = process.env.PAT;
+
+const USER_ID = "meta";
+const APP_ID = "Llama-2";
+
+const MODEL_ID = "llama2-70b-chat";
+const MODEL_VERSION_ID = "6c27e86364ba461d98de95cddc559cb3";
 
 export const nutritionist = async (req, res) => {
-    const pool = mysql.createConnection(config);
-    const connection = pool.promise();
-  
-    try {
-      const [rows] = await connection.execute(
-        "SELECT * FROM chats",
-      );
+  const { nutritionInformation } = req.body;
+  const prompt = `<s>[INST] <<SYS>>
 
-      res.status(200).json({result: rows})
-  
-    } catch (error) {
-      console.log(error);
-      res.status(500).json("Something went wrong...");
-    } finally {
-      connection.end();
-    }
-  };
+    You are a nutritionist. You have knowledge of the effects of nutrients and ingredients of a food or beverage on the body or health.
+    
+    You will be given nutritional information, ingredients, and a total quantity for a food or beverage, and you have to output the effects of that product on health in a negative manner.
+    
+    Neutrient information could be given in serving size, not in total quantity; you have to output like a person consuming the total quantity of that food or beverage.
+    
+    Use FDA recommendations.
+
+    <</SYS>>
+    
+    ${nutritionInformation}[/INST]`;
+
+  const stub = ClarifaiStub.grpc();
+
+  const metadata = new grpc.Metadata();
+  metadata.set("authorization", "Key " + PAT);
+
+  try {
+    stub.PostModelOutputs(
+      {
+        user_app_id: {
+          user_id: USER_ID,
+          app_id: APP_ID,
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID,
+        inputs: [{ data: { text: { raw: prompt } } }],
+      },
+      metadata,
+      (err, response) => {
+        if (err) {
+          throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+          throw new Error(
+            "Post model outputs failed, status: " + response.status.description
+          );
+        }
+
+        const output = response.outputs[0].data.text.raw;
+
+        res.status(200).json({ output });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Something went wrong...");
+  }
+};
